c494a43b9368e0dffb8a2ddaae8aec07ce4b66f5
Sasha Devin <sasha.devin.97@mail.ru>
2023-01-29 11:31:57 +0600

    add min

--- a_task/task2/Program.cs
+++ a_task/task2/Program.cs
@@ -1,11 +1,15 @@
 ﻿Console.Clear();
 
-int a = 5;
-int b = 7;
+int a = 145;
+int b = 43;
 
 int max = a;
+int min = a;
 
 if(b > max) max = b;
+if(b < min) min = b;
 
 Console.Write("max = ");
 Console.WriteLine(max);
+Console.Write("min = ");
+Console.WriteLine(min);
