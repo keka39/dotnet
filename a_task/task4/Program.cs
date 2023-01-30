52cef78f01cfad6f3325b8c98c18719511a0e96b
Sasha Devin <sasha.devin.97@mail.ru>
2023-01-30 09:24:51 +0600

    add enter numbers

--- a_task/task4/Program.cs
+++ a_task/task4/Program.cs
@@ -1,9 +1,12 @@
 ﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 Console.Clear();
 
-int a = 44;
-int b = 5;
-int c = 89;
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.Write("Введите третье число: ");
+int c = int.Parse(Console.ReadLine()!);
 
 int max = a;
 
