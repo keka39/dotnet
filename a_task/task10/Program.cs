4020e11ea65c4eb8b5a9592caa403b10f466c73b
Sasha Devin <sasha.devin.97@mail.ru>
2023-01-30 16:51:12 +0600

    add task10

---
+++ a_task/task10/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Clear();
+Console.WriteLine("введите число");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"число {number}");
+Console.WriteLine($"получилось число {(number % 100 - (number % 100) % 10)/10}");
