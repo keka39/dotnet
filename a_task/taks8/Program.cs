6afe403a9f3ea2c5fba9e2aaa69680f32574f711
Sasha Devin <sasha.devin.97@mail.ru>
2023-01-30 16:01:37 +0600

    add task8

---
+++ a_task/taks8/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Clear();
+Console.WriteLine("введите число");
+int number = int.Parse(Console.ReadLine()!);
+int count = 2;
+while(count <= number)
+{
+    Console.WriteLine(count);
+    count += 2;
+}
