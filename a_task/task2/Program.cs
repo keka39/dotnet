a039e02050ec4796d581f6804cb0ae734bc8afd7
Sasha Devin <sasha.devin.97@mail.ru>
2023-01-30 09:21:30 +0600

    edited

--- a_task/task2/Program.cs
+++ a_task/task2/Program.cs
@@ -3,7 +3,6 @@ Console.Clear();
 
 Console.Write("Введите первое число: ");
 int a = int.Parse(Console.ReadLine()!);
-
 Console.Write("Введите второе число: ");
 int b = int.Parse(Console.ReadLine()!);
 
