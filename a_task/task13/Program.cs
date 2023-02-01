68dcbdd48f6ec9dd6b3b6a01bf97be1c41cab41e
Sasha Devin <sasha.devin.97@mail.ru>
2023-02-01 20:39:47 +0600

    add task13

---
+++ a_task/task13/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Clear();
+Console.WriteLine("введите чило");
+int number = int.Parse(Console.ReadLine());
+
+if(number < 100 || number == 0)
+    {
+        System.Console.WriteLine("третьей цифры нет или значене равно 0");
+    }
+while(number > 1000)
+{
+    number /= 10;
+}
+if(number > 100)
+    {
+     number = number % 10;
+     System.Console.WriteLine($"третья цифра {number}");
+    }
+    
