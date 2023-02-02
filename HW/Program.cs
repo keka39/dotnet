c6c7a2f21aa78e1c014a8fe4217fe3d02b6ba60c
Sasha Devin <sasha.devin.97@mail.ru>
2023-02-02 09:47:50 +0600

    add task1 lesson 3

---
+++ HW/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+int[] numbers = {2, 5, 13, 7, 6, 4};
+float size = 6f;
+float sum = 0F;
+float avg = 0F;
+int index = 0;
+
+while(index < size)
+{
+    sum += numbers[index];
+    index++;
+}
+avg = sum / size;
+System.Console.WriteLine(avg);
