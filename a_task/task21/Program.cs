dc8d1548d2c1ae892a4648c1199cecdc52a2825d
Sasha Devin <sasha.devin.97@mail.ru>
2023-02-04 15:32:45 +0600

    add  task21

---
+++ a_task/task21/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+Console.Clear();
+Console.WriteLine("enter x1: ");
+int x1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("enter y1: ");
+int y1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("enter z1: ");
+int z1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("enter x2: ");
+int x2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("enter y2: ");
+int y2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("enter z2: ");
+int z2 = int.Parse(Console.ReadLine()!);
+
+double res = Math.Round(Math.Sqrt(Math.Pow(x2-x1, 2)+(Math.Pow(y2-y1, 2)+(Math.Pow(z2-z1, 2)))), 2);
+Console.WriteLine($"distanse {res}");
