c8f9539a7f5ffac5f78b7ac54013288d8253d9bf
Sasha Devin <sasha.devin.97@mail.ru>
2023-02-05 16:20:09 +0600

    add task03

---
+++ a_task/task03/Program.cs
@@ -0,0 +1,15 @@
+﻿//Показать кубы чисел, заканчивающихся на четную цифру
+
+Console.Clear();
+Console.WriteLine("enter nnumber");
+double N = int.Parse(Console.ReadLine()!);
+
+
+if(N % 2 == 0 || N % 2 == 2  ||N % 2 == 4 || N % 2 == 6 || N % 2 == 8)
+{
+    Console.WriteLine(Math.Pow(N, 3));
+}
+else
+{
+     Console.WriteLine("нечетное число");
+}
