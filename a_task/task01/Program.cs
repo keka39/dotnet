e9be6cd3bc132c3a3af4faea8e1e9d755c57b058
Sasha Devin <sasha.devin.97@mail.ru>
2023-02-05 15:42:40 +0600

    add task01

---
+++ a_task/task01/Program.cs
@@ -0,0 +1,17 @@
+﻿//Подсчитать сумму цифр в числе
+
+Console.Clear();
+Console.WriteLine("enter number");
+int N = int.Parse(Console.ReadLine()!);
+
+int sum = 0;
+int number = 0;
+
+
+while(N != 0)
+{
+    number = N % 10;
+    sum = sum + number;
+    N /= 10;
+}
+Console.WriteLine(sum);
