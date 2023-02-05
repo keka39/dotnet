b5efa5a77b786356593181bafb1538d5a5dbe823
Sasha Devin <sasha.devin.97@mail.ru>
2023-02-05 15:48:40 +0600

    add task02

---
+++ a_task/task02/Program.cs
@@ -0,0 +1,18 @@
+﻿//Написать программу вычисления произведения чисел от 1 до N
+
+
+Console.Clear();
+Console.WriteLine("enter number");
+int N = int.Parse(Console.ReadLine()!);
+
+int a = 1;
+int number = 0;
+
+
+while(N != 0)
+{
+    number = N % 10;
+    a = a * number;
+    N /= 10;
+}
+Console.WriteLine(a);
